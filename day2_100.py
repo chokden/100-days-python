6b6f43c3abc5b129271eba38e8d50504610af3f1
chokden <chokden@Kunga-Jigme.local>
2024-01-24 00:29:13 +0530

    Day2 - variables and string format

---
+++ day2_100.py
@@ -0,0 +1,49 @@
+"""
+Fix my Code
+
+print("Definitely not a Phishing Scam")
+print()
+myName=input("Your Name")
+print("Thanks for logging in")
+print(myName)
+cardNumber = input("What is your credit card number?")
+print("Thanks, I definitely wont put")
+print(cardNumber)
+print("into Amazon and order anything weird")
+print()
+print("Promise")
+maidenName = input("What is your Mother's maiden name? ")
+print()
+print("That's cool, I just wanted to know that")
+print(maidenName)
+print("was your Mum's maiden name. Not because the bank requested it or anything, honest.")
+"""
+
+# day2_challege
+
+print("Get to know me well today !!!")
+
+name = input("Name > ")
+age = int(input("Age >"))
+phone_os = input("iPhone or Android? >")
+message = input("""
+                  WHAT IS YOUR
+                    MESSAGE
+                       TO
+                       THE
+                      WORLD?
+"""
+)
+
+print("Well it was nice knowing you :D \n ")
+
+# use of F-strings f"{var_name} OR f"""{var_name}"""
+
+print(f""" So, you use {phone_os} and you're only {age} years old !
+Well {name}, I also use {phone_os} and I think its pretty cool O_O
+Mr.{name} message to the world is : {message}""")
+
+
+
+
+
