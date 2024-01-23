4dd5d580ffcb0cad9fdad2bc2b12ab71a84e17ad
chokden <chokdentenzin@gmail.com>
2024-01-23 13:31:57 +0000

    Day 1 Print function

---
+++ day1/main.py
@@ -0,0 +1,10 @@
+print("Tenzin Chokden")
+print("January 23rd 2024\n")
+
+print("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field! \n""")
+
+# Adding \n at the end of print statement will create a new line. 
+print("I am feeling 😝")
+print("You can follow my progress at replit.com/@chokdentenzin")
