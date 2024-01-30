89daea04479165db47f36d58ff27f73675cf85e7
chokden <chokdentenzin@gmail.com>
2024-01-30 12:04:16 +0530

    concatenation with comma (,)

---
+++ day3_100.py
@@ -0,0 +1,15 @@
+
+print("Today we will be cooking my grandma's secret recipe :D ")
+food_item = input("Write your favorite food :")
+plant_item = input("Name a unique plant :")
+cooking_style = input("Whats your cooking style ?")
+drink_item = input("Order your best drink to go with the food :")
+dine_in = input("Would you like to eat here or take home ?")
+
+print ("Let's prepare my grandma's secret recipe :D \n")
+print(f""" First, take the {food_item} and keep it on the pan on medium heat.
+                Cut the {plant_item} into four evenly shaped pieces.
+            Saute the {plant_item} on pan or you can use your style : \{cooking_style}/""")
+
+print()
+print("Serve it with your favorite drink",drink_item,"and you chose to",dine_in,". Enjoy your food :D ")
